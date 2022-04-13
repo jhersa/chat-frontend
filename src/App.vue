fc3f2dbd5179c965bb9294bc6eaaee20effaa35c
Jorge Hermosillo <jhermosillo.sa@gmail.com>
2022-04-12 23:03:38 -0600

    🚀 first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      ws: null,
+      serverUrl: "ws://localhost:5000/ws",
+    };
+  },
+
+  methods: {
+    connectToWebsocket() {
+      this.ws = new WebSocket(this.serverUrl);
+      this.ws.addEventListener("open", (event) => {
+        this.onWebsocketOpen(event);
+      });
+    },
+    onWebsocketOpen() {
+      console.log("Conectado al websocket");
+    },
+  },
+
+  mounted() {
+    this.connectToWebsocket();
+  },
+};
+</script>
+
+<template></template>
