5c693e6eadf301c63a833a42ce1afa2a4e059188
Jorge Hermosillo <jhermosillo.sa@gmail.com>
2022-04-20 00:06:21 -0600

    inteto de uso de bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import { createApp } from "vue";
-
 import App from "./App.vue";
 
-const app = createApp(App);
-
-app.mount("#app");
+createApp(App).mount("#app");
